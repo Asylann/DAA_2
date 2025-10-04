dabc52df51a6c6507c356b29b5769feccb50a62b
Asylann <usenasylan@gmail.com>
2025-10-04 13:24:23 +0500

    proper PerfomanceTracker added

---
+++ src/main/java/metrics/PerformanceTracker.java
@@ -0,0 +1,60 @@
+package metrics;
+
+public class PerformanceTracker {
+    private long comparisons;
+    private long arrayAccesses;
+    private long startTime;
+    private long endTime;
+
+    public PerformanceTracker() {
+        reset();
+    }
+
+    public void reset() {
+        comparisons = 0;
+        arrayAccesses = 0;
+        startTime = 0;
+        endTime = 0;
+    }
+
+    public void startTimer() {
+        startTime = System.nanoTime();
+    }
+
+    public void stopTimer() {
+        endTime = System.nanoTime();
+    }
+
+    public void incrementComparison() {
+        comparisons++;
+    }
+
+    public void incrementArrayAccess() {
+        arrayAccesses++;
+    }
+
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public long getArrayAccesses() {
+        return arrayAccesses;
+    }
+
+    public long getExecutionTimeNanos() {
+        return endTime - startTime;
+    }
+
+    public double getExecutionTimeMillis() {
+        return getExecutionTimeNanos() / 1000000.0;
+    }
+
+    public String exportToCsv(int inputSize) {
+        return String.format("%d;%d;%d;%.3f", 
+            inputSize,
+            comparisons,
+            arrayAccesses,
+            getExecutionTimeMillis()
+        );
+    }
+}
